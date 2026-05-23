fe5530e86995a5bfbcae9d6ef96890ef7e4e87ba
EL-ADARISSI <el.adarissiabdelaziz@gmail.com>
2026-05-23 23:07:43 +0100

    Front-end packages are setup

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+import 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      Front-end of the application is running!
+    </div>
+  )
+}
+
+export default App
